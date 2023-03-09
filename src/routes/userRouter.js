7d66da4d8123be6615a51c8a972bf9792ba41a06
dccuong <cuongksv10@mail.com>
2023-03-09 22:13:10 +0700

    fix api size

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,9 +4,9 @@ import { isAdmin, isAuth, requireSignin } from "../middlewares/checkAuth";
 
 const router = Router();
 
-router.post("/users", requireSignin, isAuth, isAdmin, create);
+router.post("/users", create);
 router.get("/users", getAll);
-router.get("/users/:id", requireSignin, isAuth, isAdmin, get);
+router.get("/users/:id", get);
 router.put("/users/changepassword/:id", requireSignin, isAuth, changePassword);
 router.put("/users/:id", requireSignin, isAuth, isAdmin, update);
 router.delete("/users/:id", requireSignin, isAuth, isAdmin, remove);
