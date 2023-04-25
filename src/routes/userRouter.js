9e43440096d545c0bfd782397981ebd8e63b00f5
dccuong <cuongksv10@mail.com>
2023-04-25 22:19:50 +0700

    update

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -5,7 +5,7 @@ const router = Router();
 router.post("/users", create);
 router.get("/users", getAll);
 router.get("/users/:id", get);
-router.put("/users/changepassword/:id", requireSignin, changePassword);
+router.put("/users/changepassword/:id", changePassword);
 router.put("/users/:id",  update);
 router.delete("/users/:id", requireSignin, isAuth, remove);
 
